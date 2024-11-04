7e59eed4658d1ebb662ece461ba83ceebd365fe0
Nameless Coder <110830345+Haarinaath-Y@users.noreply.github.com>
2024-11-04 23:52:43 +0530

    Add files via upload

--- pages/3_✏_Admin.py
+++ pages/3_✏_Admin.py
@@ -25,7 +25,7 @@ def main():
                 st.warning(f"Please select the project in Home Page !!")
                 print(f'Error log: {e}')
         except Exception as e:
-            st.warning(f"Please login with Google in Home Page!!")
+            st.warning(f"No data available for the selected project!!")
             print(f'Error log: {e}')
     except Exception as e:
         st.warning(f"Please login with Google in Home Page!!")
